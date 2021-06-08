df6d4b69f72cf0ebaca60027f31d8f0db3d51184
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-06-08 15:27:09 +0500

    Redux setup with auth Reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { login, logout } from "./redux/reducers/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.rootReducer.auth.isLoggedIn);
+  console.log(isLoggedIn);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch(isLoggedIn ? logout() : login())}>
+        {isLoggedIn ? "LOGOUT" : "LOGIN"}
+      </button>
     </div>
   );
 }
