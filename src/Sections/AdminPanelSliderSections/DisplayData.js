76fd9379bf05fcba4646d9dd99b985a9db36e56a
ameera-saleem <ameerasaleem430@gmail.com>
2021-06-08 16:51:34 -0700

    added section folder for home slider panel

---
+++ src/Sections/AdminPanelSliderSections/DisplayData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Grid,Typography,Button,List,ListItem} from "@material-ui/core"
+const DisplayData = () => {
+    return (
+        <div>
+            <h1>DisplayData</h1>
+        </div>
+    )
+}
+
+export default DisplayData
