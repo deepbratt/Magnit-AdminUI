b3978e4d9a6760d26f2ec150945cdca75fec509d
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-06-10 20:27:26 +0500

    Update addNewUser.js

--- src/pages/users/addNewUser.js
+++ src/pages/users/addNewUser.js
@@ -1,25 +1,91 @@
 import PropTypes from "prop-types";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItem from "@material-ui/core/ListItem";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import FullPageDialog from "../../components/FullPageDialog";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Grid, InputLabel, MenuItem, TextField } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    minWidth: "100%",
+    padding: "20px",
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      padding: "10px",
+    },
+  },
+  multipleInput: {
+    minWidth: "100%",
+  },
+  buttonWrap: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+  },
+}));
+
+const roles = ["Admin", "SE0 Professional", "Developer"];
 
 const AddNewUser = ({ open, handleClose }) => {
+  const classes = useStyles();
+
   return (
     <FullPageDialog open={open} handleClose={handleClose}>
-      <List>
-        <ListItem button>
-          <ListItemText primary="Phone ringtone" secondary="Titania" />
-        </ListItem>
-        <Divider />
-        <ListItem button>
-          <ListItemText
-            primary="Default notification ringtone"
-            secondary="Tethys"
-          />
-        </ListItem>
-      </List>
+      <Grid container>
+        <Grid className={classes.formRoot} container item xs={12}>
+          <form className={classes.form}>
+            <Grid item xs={12} md={6}>
+              <InputLabel id="input-title">Title</InputLabel>
+              <TextField
+                labelId="input-title"
+                id="input-title"
+                variant="outlined"
+                placeholder="Careers | The Magnit"
+                size="medium"
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <InputLabel id="input-canonical">Canonical</InputLabel>
+              <TextField
+                labelId="input-canonical"
+                id="input-canonical"
+                variant="outlined"
+                placeholder="/careers"
+                size="medium"
+                fullWidth
+                required
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <InputLabel id="input-query">Query</InputLabel>
+              <TextField
+                id="filled-select-currency-native"
+                select
+                fullWidth
+                variant="outlined"
+              >
+                {roles.map((option) => (
+                  <MenuItem key={`option-${option}`} value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid className={classes.buttonWrap} item xs={12} md={6}>
+              <Button
+                style={{ maxWidth: "100px" }}
+                variant="contained"
+                color="secondary"
+                size="large"
+              >
+                Delete
+              </Button>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
     </FullPageDialog>
   );
 };
