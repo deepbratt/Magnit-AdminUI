62fa4f49491f17e0974056d021e7173606ef946b
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-06-16 18:56:43 +0500

    removed component from testing

--- src/pages/ModerateSections/index.js
+++ src/pages/ModerateSections/index.js
@@ -1,10 +1,8 @@
 import { Typography } from "@material-ui/core";
-import HowItWorks from '../../Sections/HowItWorks'
 const ModerateSections = () => {
   return (
     <div>
       <Typography>Manage, Sections/Components</Typography>
-      <HowItWorks/>
     </div>
   );
 };
