4ffe51773336dc5e314b5afffec5daca458ce6ae
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-06-22 20:52:05 +0500

    removed test sections from home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,9 @@
 import { Typography } from "@material-ui/core";
-import CaseStudies from "../../Sections/CaseStudies";
-import HowItWorks from "../../Sections/HowItWorks";
 
 const Home = () => {
   return (
     <div>
       <Typography>Hello, Admin</Typography>
-      <CaseStudies/>
-      {/* <HowItWorks/> */}
     </div>
   );
 };
