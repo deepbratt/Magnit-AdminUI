b0dcb122e2f66347e90e702f146dea25011d2bcb
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-07-17 20:33:57 +0500

    added unauthorizing function

--- src/Utils/helperFunctions.js
+++ src/Utils/helperFunctions.js
@@ -1,6 +1,12 @@
-export const isResponseSuccess = (response) =>{
-    if(response && response.data && response.data.status==="success"){
-        return true
-    }
-    return false
-}
+export const isResponseSuccess = (response) => {
+  if (response && response.data && response.data.status === "success") {
+    return true;
+  }
+  return false;
+};
+export const isUnauthorized = (response) => {
+  if (response.response.status === 401) {
+    return true;
+  }
+  return false;
+};
